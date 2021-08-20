7a878599d8bcc630a44f07ee9a2baf7d335938dd
Oleksii Zvedeniuk <zvedalex@gmail.com>
2021-08-20 09:26:33 +0300

    update

--- weather_bot.py
+++ weather_bot.py
@@ -157,7 +157,7 @@ def send_additional_forecast(call):
 
 
 @app.route('/' + BOT_TOKEN, methods=['POST'])
-def get_upadtes():
+def get_upadates():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
