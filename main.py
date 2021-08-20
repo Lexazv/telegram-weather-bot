e4398b6bdfb009eae8ffc05f2a1a202bdc4b46e9
Oleksii Zvedeniuk <zvedalex@gmail.com>
2021-08-20 16:24:02 +0300

    test commit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-
+import os
 import logging
 from datetime import date
 
 import telebot
 from telebot import types
+from flask import Flask, request
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, APP_URL
 from url_functions import *
 from weather_bot_phrases import bot_phrases
 
 bot = telebot.TeleBot(BOT_TOKEN)
+app = Flask(__name__)
 
 
 logging.basicConfig(
@@ -154,5 +156,20 @@ def send_additional_forecast(call):
         bot.reply_to(call.message, bot_phrases["connection error"])
 
 
+@app.route("/" + BOT_TOKEN, methods=["POST"])
+def get_update():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@app.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL.format(BOT_TOKEN))
+    return "!", 200
+
+
 if __name__ == '__main__':
-    bot.polling()
+    app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
